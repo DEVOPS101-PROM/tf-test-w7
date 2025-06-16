f3af61212b3989e671324322ecac549b9d0643cb
Tirtxika <zhuk.viacheslav@gmail.com>
2025-06-16 15:20:10 +0300

    Refactor GKE module: streamline node pool configuration by consolidating parameters in main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,7 @@ module "gke_cluster" {
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
-  resource {
-    name       = "main-node-pool"
-    delete_default_node_pool = true
-    node_count = var.GKE_NUM_NODES
-    node_config {
-      machine_type = "e2-micro"
-    }
-  }
+  node_pool_name = "main-node-pool"
+  machine_type   = "e2-micro"
+  delete_default_node_pool = true
 }
